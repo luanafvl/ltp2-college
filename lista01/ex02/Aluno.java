f28110b14cebde995bdb1ebf23869e47e5a6d938
luhh_fvl <168596833+luanafvl@users.noreply.github.com>
2025-04-13 21:38:50 -0300

    Create Aluno.java

---
+++ lista01/ex02/Aluno.java
@@ -0,0 +1,12 @@
+package entities;
+public class Aluno {
+	String nome;
+	String matricula;
+	
+	public Aluno() {}
+	
+	public Aluno(String nome, String matricula) {
+		this.nome = nome;
+		this.matricula = matricula;
+	}
+}
