89046f0070a969d9c220dfcfa188fe4a89f7c5b7
luhh_fvl <168596833+luanafvl@users.noreply.github.com>
2025-04-14 14:45:49 -0300

    Create Program.java

---
+++ lista02/ex08/Program.java
@@ -0,0 +1,14 @@
+package lista02;
+import entities.ObjetoContado;
+public class Program {
+  
+	public static void main(String[] args) {
+  	 	
+		ObjetoContado o1 = new ObjetoContado();
+		ObjetoContado o2 = new ObjetoContado();
+		ObjetoContado o3 = new ObjetoContado();
+		ObjetoContado o4 = new ObjetoContado();
+		
+		System.out.println(ObjetoContado.contador);
+	}
+}
