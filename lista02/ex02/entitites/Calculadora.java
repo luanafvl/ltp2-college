7b8b50f296f2f52699497798d56fa9e8b9bbd024
luhh_fvl <168596833+luanafvl@users.noreply.github.com>
2025-04-14 14:35:52 -0300

    Create Calculadora.java

---
+++ lista02/ex02/entitites/Calculadora.java
@@ -0,0 +1,15 @@
+package entities;
+public class Calculadora {
+	
+	public Integer soma(Integer a, Integer b) {
+		return a + b;
+	}
+	
+	public Double soma(Double a, Double b) {
+		return a + b;
+	}
+	
+	public Integer soma(Integer a, Integer b, Integer c) {
+		return a + b + c;
+	}
+}
