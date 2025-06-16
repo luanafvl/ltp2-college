57a6c05be306c597ff6d8f09fe24140d85c1c004
luhh_fvl <168596833+luanafvl@users.noreply.github.com>
2025-06-15 23:05:28 -0300

    Update ClienteDao.java
    Mudança nos parâmetros e inclusão de comentários

--- trabalho01_empresa_viagens/src/model/dao/ClienteDao.java
+++ trabalho01_empresa_viagens/src/model/dao/ClienteDao.java
@@ -1,10 +1,39 @@
+// Define o pacote onde está a interface
 package model.dao;
 
+import model.entities.Cliente;
+
+// Interface que representa o contrato para operações de acesso a dados da entidade Cliente
 public interface ClienteDao {
 
-	void insert();
-	void update();
-	void deleteById();
-	void findById();
+	/**
+     	* Insere um novo cliente no banco de dados.
+     	* @param pacote O objeto Cliente a ser inserido.
+     	*/
+	void insert(Cliente obj);
+
+	/**
+     	* Atualiza os dados de um cliente existente no banco.
+     	* @param O id do usuário a ser atualizado e o objeto com os dados atualizados.
+     	*/
+	void update(Integer id, Cliente obj);
+
+	/**
+     	* Remove um cliente do banco com base no ID.
+     	* @param id O ID do pacote que deve ser removido.
+     	*/
+	void deleteById(Integer id);
+
+	/**
+    	 * Busca um cliente pelo seu ID.
+     	* @param id O ID do pacote que deve ser buscado.
+     	* @return O objeto Cliente correspondente, ou null se não encontrado.
+     	*/
+	Cliente findById(Integer id);
+
+	/**
+    	 * Retorna todos os clientes cadastrados.
+     	* @return Lista de Cliente.
+     	*/
 	void findAll();
 }
